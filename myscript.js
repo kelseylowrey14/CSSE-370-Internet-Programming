ec7b8b83483cffa018a2e054103cb09bb70f0e3e
Kelsey <kelseylowrey14@gmail.com>
2020-04-02 17:19:04 -0700

    Star wars fix

--- myscript.js
+++ myscript.js
@@ -18,56 +18,29 @@ function myMoveFunction() {
   document.getElementById("demo").innerHTML = z+=1;
 }
 
-/*function main() {
-    document.getElementById("LOTRAPI").innerHTMl = "Hello LOTR";
-}
-function fetchLOTR(){
-    fetch("https://the-one-api.herokuapp.com/v1/book", {
-        method: "GET",
-        headers: {
-            "Accept":"application/json",
-            "Content-Type": "application/json",
-            "Authorization":"Bearer ci3sASKvz3hCGN10OHn3"
-        }
-    }).then(resp->resp.json()).then(processLOTR).cath(err ->console.log(err))
-}
-
-function processLOTR(jsonLOTR){
-   var book = document.getElementById("book") 
-   jsonLOTR
-    .data
-    .filter(jsonLOTR->jsonLOTR.book.name)
-    .forEach(book->{
-            console.log(book.name)
-            var li = document.createElement('li')
-            li.appendChild(document.createTextNode(book.name))
-    book.appendChild(li)
-             });
-}
-*/
 
 function main() {
     document.getElementById("demo2").innerHTML = "Star Wars people!";
 }
 function fetchPerson() {
     fetch("https://swapi.co/api/people/", {
-        method: "GET",mode:'no-cors',
+        method: "GET",
+        mode:'no-cors',
         headers: {
             "Accept": "application/json",
             "Content-Type": "application/json",
-            
         }
-    }).then(resp => resp.json()).then(processperson).catch(err=>console.log(err))
+    }).then(resp => resp.json()).then(processPeople)
 }
 
-function processperson(jsonperson) {
-    var elperson = document.getElementById("name")
-    jsonperson
+function processPeople(jsonPeople) {
+    var elperson = document.getElementById("person")
+    jsonPeople
         .data
         .forEach(person => {
-        console.log(results.name)
+            console.log(person.name)
             var li = document.createElement('li')
-            li.appendChild(document.createTextNode(results.name))
+            li.appendChild(document.createTextNode(person.name))
             elperson.appendChild(li)
         });
 }
